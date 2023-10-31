5f57305ac3c18d7dcc00ea9059ed1402bc0c0403
MiroMuro <miro.laukkanen44@gmail.com>
2023-10-31 17:19:34 +0200

    Added fetching weather data by cityname

--- WeatherApplication/src/weatherApplication/WeatherApplicationBackend.java
+++ WeatherApplication/src/weatherApplication/WeatherApplicationBackend.java
@@ -13,23 +13,61 @@ import org.json.simple.parser.ParseException;
 public class WeatherApplicationBackend {
 	
 	 public static JSONObject getWeatherData(String locationName) {
-		 
-		 JSONArray locationData = (JSONArray) getLocationData(locationName);
-		 
+		 //Fetching of geodata with city name
+		 JSONArray locationData = (JSONArray) getCityLocationData(locationName);
+		 //JSONObject that holds latitude, longitude, country code and the population of the searched city
 		 JSONObject cityData = (JSONObject)locationData.get(0);
-		 System.out.println(cityData);
 		 
-		 System.out.println(cityData.get("country_code"));
-		 System.out.println(cityData.get("population"));
-		 System.out.println(cityData.get("latitude"));
-		 System.out.println(cityData.get("longitude"));
+		 //
+		 double latitude = (double)cityData.get("latitude");
+		 double longitude = (double) cityData.get("longitude");
+		 System.out.println("latitude "+latitude+"longitude "+longitude);
+		 long population = (long) cityData.get("population");
+		 //Country code for fetching the image of the countrys flag.
+		 String CC = (String) cityData.get("country_code");
+		 
+		 //Api url that returns weather info based on the geolocation parameters.
+		 String weatherUrlString = "https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+
+		 longitude+"&current=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&daily=weathercode,sunrise,sunset&timezone=Europe%2FMoscow";
+		 
+		 //Lets call the api and gather the results.
+		 
+		 
+		 
+		 try {
+			HttpURLConnection conn = fetchApiResponse(weatherUrlString);
+			if(conn.getResponseCode()!= 200) {
+				 System.out.println("API fetch error.");
+				 return null;
+			 }else {
+				 StringBuilder fetchedJson = new StringBuilder();
+				 Scanner scanner = new Scanner(conn.getInputStream());
+				 
+				 while(scanner.hasNextLine()) {
+					 fetchedJson.append(scanner.nextLine());
+				 }
+				 scanner.close();
+				 conn.disconnect();
+				 
+				 JSONParser parser = new JSONParser();
+				 JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(fetchedJson));
+				 System.out.println("current: "+resultsJsonObj.get("current"));
+			 }
+		} catch (IOException | ParseException e ) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+		 
+		 
+		 
 		 
 		 
 		 return null;
 		 
 	 }
 	 
-	 public static JSONArray getLocationData(String locationName)  {
+	 public static JSONArray getCityLocationData(String locationName)  {
 		 String urlString = "https://geocoding-api.open-meteo.com/v1/search?name="+
 				 			locationName+"&count=10&language=en&format=json";
 		 try{
